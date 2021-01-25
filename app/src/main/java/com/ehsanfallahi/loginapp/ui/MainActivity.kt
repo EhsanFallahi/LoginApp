7a46f74286ff51307f3af122d21ea498eb815a52
Ehsan Fallahi <ehsanfallahi@gmail.com>
2021-01-25 12:01:14 +0330

    add  requirement dependency

---
+++ app/src/main/java/com/ehsanfallahi/loginapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ehsanfallahi.loginapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ehsanfallahi.loginapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
